e2d81d56c67a15d054f6c838651c6f8f2b1f6c56
Kishan <kishanland@gmail.com>
2022-01-24 10:55:16 +0530

    Main Navigation added

--- meetup/src/App.js
+++ meetup/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/new-meetup" element={<NewMeetup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
